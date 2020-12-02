4226aeac1a7473e596d36c097c1e0f1c4552c22b
Dimon-228 <74213423+Dimon-228@users.noreply.github.com>
2020-12-02 15:49:32 +0200

    Add files via upload

---
+++ Lesson 1/Task 7/task7/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  char b = ',';
+
+  cout << "Vvedite chislo: ";
+  cin >> a;
+
+    for (int i = 0; i <= a; i++){
+            cout << i;
+                while(i < a){
+                cout << b;
+                break;
+
+
+                }
+    }
+
+
+
+}
+
